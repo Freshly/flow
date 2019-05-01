c2d3f8abeefa6a9b412978fcdd3b6d31c86d8f3d
Jordan Minneti <jkminneti@gmail.com>
2019-05-01 13:18:53 -0400

    very WIP

--- lib/flow/custom_matchers.rb
+++ lib/flow/custom_matchers.rb
@@ -9,9 +9,14 @@ require_relative "custom_matchers/handle_error"
 require_relative "custom_matchers/use_operations"
 require_relative "custom_matchers/wrap_in_transaction"
 require_relative "custom_matchers/have_failure"
+require_relative "custom_matchers/have_on_state"
 
 module CustomMatchers
   def have_failure(problem)
     HaveFailure.new(problem)
+  end
+
+  def have_on_state(state_stuff)
+    HaveOnState.new(state_stuff)
   end
 end
