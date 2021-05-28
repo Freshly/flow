905d30604738a52dc1ce399f87fd642512f8f770
Allen Rettberg <allen.rettberg@freshly.com>
2021-05-27 21:52:34 -0400

    use Substance instead of Spicerack objects, if they're available

--- lib/flow.rb
+++ lib/flow.rb
@@ -4,18 +4,22 @@ require "active_model"
 require "active_record"
 require "active_support"
 
-require "spicery"
+require "spicerack"
 require "malfunction"
 
-require "flow/version"
+require_relative "flow/version"
 
 require_relative "flow/errors"
 
-require "flow/concerns/transaction_wrapper"
+require_relative "flow/concerns/transaction_wrapper"
 
-require "flow/malfunction/base"
+require_relative "flow/malfunction/base"
 
-require "flow/flow_base"
-require "flow/operation_base"
-require "flow/state_base"
-require "flow/state_proxy"
+# TODO: Remove inheritance nonsense, just use Substance once deprecation is removed from Spicerack
+class Flow::RootObject < (defined?(Substance::RootObject) ? Substance::RootObject : Spicerack::RootObject); end
+class Flow::InputModel < (defined?(Substance::InputModel) ? Substance::InputModel : Spicerack::InputModel); end
+
+require_relative "flow/flow_base"
+require_relative "flow/operation_base"
+require_relative "flow/state_base"
+require_relative "flow/state_proxy"
